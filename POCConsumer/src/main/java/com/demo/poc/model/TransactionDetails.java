1d19a0433d2f38d2649b9ee7105634213b89a2dc
Yatindra <Yatindra@INPUHJPC09950.TechMahindra.com>
2018-09-18 14:06:17 +0530

    change naming convention

---
+++ POCConsumer/src/main/java/com/demo/poc/model/TransactionDetails.java
@@ -0,0 +1,58 @@
+package com.demo.poc.model;
+import java.util.Date;
+
+public class TransactionDetails {
+
+	private Long txn_id;	
+	private Integer device_number;
+	private Integer txn_amount;
+	private String device_type;
+	private Date txn_date;
+		 
+	public Long getTxn_id() {
+		return txn_id;
+	}
+
+	public void setTxn_id(Long txn_id) {
+		this.txn_id = txn_id;
+	}
+
+	public Integer getDevice_number() {
+		return device_number;
+	}
+
+	public void setDevice_number(Integer device_number) {
+		this.device_number = device_number;
+	}
+
+	public Integer getTxn_amount() {
+		return txn_amount;
+	}
+
+	public void setTxn_amount(Integer txn_amount) {
+		this.txn_amount = txn_amount;
+	}
+
+	public String getDevice_type() {
+		return device_type;
+	}
+
+	public void setDevice_type(String device_type) {
+		this.device_type = device_type;
+	}
+
+	public Date getTxn_date() {
+		return txn_date;
+	}
+
+	public void setTxn_date(Date txn_date) {
+		this.txn_date = txn_date;
+	}
+
+	@Override
+	public String toString() {
+		return "TransactionDetails [txn_id=" + txn_id + ", device_number=" + device_number + ", txn_amount="
+				+ txn_amount + ", device_type=" + device_type + ", txn_date=" + txn_date + "]";
+	}
+	
+} 
